1fa1d3e959ddfedd8d959aa299baf2c08564ae46
Jorge Arellano <mascu@JArellano.localdomain>
2026-03-10 12:04:01 -0700

    Add file reading for SIC and ignore compiled executable

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,39 @@
 // main.cpp
 // CS530 Project 2 - SIC/XE Assembler
-// Jorge Arellano
-// Manuel Jimenez
-
 #include <iostream>
+#include <fstream>
+#include <string>
+
 
 int main(int argc, char* argv[]) {
-    std::cout << "LXE Assembler starting..." << std::endl;
+
+    std::cout << "LXE assembler starting...\n";
+
+    if(argc < 2){
+        std::cout << "Usage: ./lxe file1.sic file2.sic\n";
+        return 1;
+    }
+
+
+    for(int i = 1; i < argc; i++){
+
+    std::ifstream infile(argv[i]);
+
+    if(!infile){
+        std::cout << "Error opening file: " << argv[i] << std::endl;
+        continue;
+    }
+
+    std::cout << "Processing file: " << argv[i] << std::endl;
+
+    std::string line;
+
+    while(std::getline(infile, line)){
+        std::cout << line << std::endl;
+    }
+
+    infile.close();
+}
+
     return 0;
 }
