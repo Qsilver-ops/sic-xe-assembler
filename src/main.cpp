6efd5cd9ce1a73b16682e31f091e2501b05a74e3
Qsilver-ops <Masculus01@yahoo.com>
2026-03-10 09:56:41 -0700

    Initialize main.cpp for SIC/XE Assembler project
    added main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+// main.cpp
+// CS530 Project 2 - SIC/XE Assembler
+// Jorge Arellano
+// Manuel Jimenez
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "LXE Assembler starting..." << std::endl;
+    return 0;
+}
